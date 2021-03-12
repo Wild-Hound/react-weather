48e37542cfc4d32279173a1a8b0cfaa233f151a7
Hound404 <wildhound404@gmail.com>
2021-03-12 21:49:46 +0600

    deploy

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import mainBg from '../Images/main_bg.jpeg'
 
 function CurrentWeather(props) {
     const {feels_like,weather,temp,humidity,wind_speed} = props.data
     
     return (
-        <div>
-            <h1>Today({props.day}): {temp}&#8451; {weather[0].description.toUpperCase()}</h1>
-            <h3>Feels Like: {feels_like}&#8451;</h3>
-            <h3>Humidity: {humidity}%</h3>
-            <h4>Wind Speed: {wind_speed}km/h</h4>
+        <div className="currentWeather">
+            <div>
+                <h1>Today({props.day})<br/> {temp}&#8451; {weather[0].description.toUpperCase()}</h1>
+                <h3>Feels Like: {feels_like}&#8451;</h3>
+                <h3>Humidity: {humidity}%</h3>
+                <h4>Wind Speed: {wind_speed}km/h</h4>
+            </div>
         </div>
     )
 }
